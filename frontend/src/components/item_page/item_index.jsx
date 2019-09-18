0136930da0c6ca5f34813b6f6bd6722cf7aeb915
Carlos Catly <ccatly@gmail.com>
2019-09-18 13:55:52 -0700

    started index page and modified switch routes

--- frontend/src/components/item_page/item_index.jsx
+++ frontend/src/components/item_page/item_index.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import ItemIndexItem from './item_index_item';
 
 class ItemIndex extends React.Component {
+	componentDidMount(){
+		this.props.fetchItems()
+	}
+
 	render() {
+		const { items } = this.props
+
 		return (
-			<div></div>
+			<div className="item-index">
+				<div className="item-lists">
+					<ul>
+						{items.map(item => <ItemIndexItem key={item.id} item={item}/>)}
+					</ul>
+				</div>
+			</div>
 		)
 	}
 }
---
+++ frontend/src/components/item_page/item_index_item.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ItemIndexItem extends Component {
+    render(){
+        const { item } = this.props
+        return(
+            <li>
+                
+            </li>
+        )
+    }
+}
+
+export default ItemIndexItem
