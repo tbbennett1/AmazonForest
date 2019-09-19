ae3146255fd2a3656247630ee132194f2c8a8d83
Abby Xu <abby.tsui2016@gmail.com>
2019-09-19 11:26:57 -0700

    adding cart container

--- frontend/src/components/cart_page/cart_index.jsx
+++ frontend/src/components/cart_page/cart_index.jsx
@@ -12,36 +12,14 @@ class CartIndex extends React.Component {
 	render() {
 		const { cartItems } = this.props;
 
-		const myCart = cartItems.map((cartItem) => {
-			return (
-				<div>
-					< CartIndexItem key={cartItem.id} cartItem={cartItem} />
-				</div>
-			);
+		const myCartItems = cartItems.map((cartItem) => {
+			return < CartIndexItem key={cartItem.id} cartItem={cartItem} />
 		});
 
 		return (
-			<div className="board-canvas">
-
-				<div className="create">
-					<div className="create-option">
-						<Link to="/create-pin">Create Pin</Link>
-						<Link to="/create-board">Create Board</Link>
-					</div>
-				</div>
-
-				<div className="myName">{currentUser.username}</div>
-
-				<div className="dashboard-nav">
-					<a href="">Boards</a>
-					<a href="">Pins</a>
-					<a href="">Topics</a>
-					<a href="">Hardcode</a>
-				</div>
-
-				<div className="board-frame">
-					{myBoards}
-				</div>
+			<div>
+				<p>Saved for later - { myCartItems.length } items</p>
+				{ myCartItems }
 			</div>
 		)
 	}
