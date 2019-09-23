5431cbfcd0bb8c407ae549b5f5d52678a183b242
bbennett <tbbennett1@gmail.com>
2019-09-23 15:16:36 -0700

    add user profile/user product listings

--- frontend/src/components/item_page/item_index_container.jsx
+++ frontend/src/components/item_page/item_index_container.jsx
@@ -4,8 +4,15 @@ import { fetchItems, fetchItem, removeItem } from "../../actions/item_actions";
 
 import ItemIndex from './item_index';
 
-const mapStateToProps = (state) => {
-	
+const mapStateToProps = (state, ownProps) => {
+  if (ownProps.userId && state.entities.items.data){
+    let usersItems = [];
+    usersItems[0] = state.entities.items.data.filter(item => item.sellerId === ownProps.userId);
+    return{
+      items: usersItems
+    }
+  }
+  
 	return {
 		items: Object.keys((state.entities.items)).map(id => state.entities.items[id])
 	}
