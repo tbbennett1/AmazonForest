7ebdbd1fed088c153580a3f96111638fa16ffa56
bbennett <tbbennett1@gmail.com>
2019-09-24 16:29:29 -0700

    trying to fix delete button for user profile

--- frontend/src/components/user_profile/user_profile_cart.jsx
+++ frontend/src/components/user_profile/user_profile_cart.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import ItemIndexItem from '../item_page/item_index_item';
+import CartIndexItem from '../cart_page/cart_index_item';
 
 class UserProfileCart extends React.Component{
   render(){
-    let itemLists;
-    if (this.props.items) {
-      itemLists = this.props.items.map(item => <ItemIndexItem key={item._id} item={item} />)
-    }
+    debugger
+    let itemLists = this.props.items.map((cartItem) => {
+      debugger
+      let item = this.props.items[cartItem.itemId];
+      return <CartIndexItem 
+              key={cartItem._id}
+              cartItem={cartItem}
+              item={item}
+              removeCartItem={this.props.removeCartItem} />
+    });
+
     if(itemLists.length === 0){
       itemLists = <h3>You currently have 0 cart items</h3>;
     }
     return(
       <div className="user-profile-cart">
         <Link to="/cart"><h1>Your Cart</h1></Link>
-        <ul className="ul-ul">
+        <ul className="ul-uc">
           {itemLists}
         </ul>
       </div>
