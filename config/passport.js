de38cce9106fcdab28da2d37784b350485421e15
Brett Meyer <meyer.brett.da@gmail.com>
2019-10-24 16:07:48 -0700

    m

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 const User = mongoose.model('users');
-const keys = require('../config/keys_dev.js');
+const keys = require('./keys.js');
 
 const options = {};
 options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
