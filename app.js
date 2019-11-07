157d5e89987a3d4685424cc8b190937856b54828
Carlos Catly <ccatly@gmail.com>
2019-11-06 16:53:13 -0800

    changed back the express server for heroku

--- app.js
+++ app.js
@@ -33,9 +33,9 @@ app.use("/api/items", items);
 app.use("/api/reviews", reviews);
 app.use("/api/cartitems", cartItems);
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-// app.listen(process.env.PORT || 3000, function () {
-//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-// });
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
